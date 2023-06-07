64b3e0a5594029a7bb6b09800c57c1a7e1b8395a
amyrhuerta <89152622+amyrhuerta@users.noreply.github.com>
2023-06-06 19:10:42 -0600

    Update evidenciafinal_a01702927.py

--- evidenciafinal_a01702927.py
+++ evidenciafinal_a01702927.py
@@ -7,7 +7,8 @@ import numpy as np
 #import matplotlib.pyplot as plt
 
 st.header(':blue[Police Incident Reports from 2018 to 2020 in San Francisco] :police_car: :us:')
-df=pd.read_csv("Police.csv")
+df=pd.read_csv("/Users/Amy Rdz/Downloads/Police.csv")
+st.dataframe(df)
 
 st.markdown("_The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution._")
 
