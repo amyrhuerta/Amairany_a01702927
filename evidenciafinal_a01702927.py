a7c687deb8553c038862b8853fabddfb1e2db284
amyrhuerta <89152622+amyrhuerta@users.noreply.github.com>
2023-06-06 18:47:35 -0600

    Update evidenciafinal_a01702927.py

--- evidenciafinal_a01702927.py
+++ evidenciafinal_a01702927.py
@@ -11,7 +11,7 @@ df=pd.read_csv("https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67u
 
 st.markdown("_The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution._")
 
-mapa=pd.DataFRame()
+mapa=pd.DataFrame()
 mapa['Date']=df["Incident Date"]
 mapa['Day']=df["Incident Day of Week"]
 mapa['Neighborhood']=df["Analysis Neighborhood"]
