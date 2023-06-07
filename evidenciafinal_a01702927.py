b51e74452ae91e7abc3977fc1a484c81226e2de6
amyrhuerta <89152622+amyrhuerta@users.noreply.github.com>
2023-06-06 18:44:29 -0600

    Update evidenciafinal_a01702927.py

--- evidenciafinal_a01702927.py
+++ evidenciafinal_a01702927.py
@@ -20,7 +20,7 @@ from bokeh.plotting import figure
 import matplotlib.pyplot as plt
 
 st.header(':blue[Police Incident Reports from 2018 to 2020 in San Francisco] :police_car: :us:')
-df=pd.read_csv("https://drive.google.com/file/d/11oLcKiW8SgCOp3tGiQCYuRG7pLL_J-Zf/view?usp=share_link/Police_Department_Incident_Reports__2018_to_Present.csv")
+df=pd.read_csv("https://drive.google.com/file/d/1WQ5QFkP9S1FfpAxCTxQpzWKEP5lD67uL/view?usp=share_link/Police.csv")
 
 st.markdown("_The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution._")
 
@@ -35,6 +35,6 @@ mapa['lat']=df["Latitude"]
 mapa['lon']=df["Longitude"]
 
 mapa=mapa.dropna()
-st.map(mapa)
+st.map(mapa.astype(int))
 
 st.write(f'Total Records: {len(df)}')
