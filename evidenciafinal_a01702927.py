89daff660198a9ceeb965e76f36f1e614febbccb
amyrhuerta <89152622+amyrhuerta@users.noreply.github.com>
2023-06-06 22:00:07 -0600

    Update evidenciafinal_a01702927.py

--- evidenciafinal_a01702927.py
+++ evidenciafinal_a01702927.py
@@ -7,10 +7,9 @@ import numpy as np
 #import matplotlib.pyplot as plt
 
 st.header(':blue[Police Incident Reports from 2018 to 2020 in San Francisco] :police_car: :us:')
+st.markdown("_The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution._")
 df=pd.read_csv("https://github.com/amyrhuerta/Amairany_a01702927/blob/c010d2d44ddb43506255da1551f75b20e8453543/Police.csv")
 st.dataframe(df)
-
-st.markdown("_The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution._")
 
 mapa=pd.DataFrame()
 mapa['Year']=df["Incident Year"]
